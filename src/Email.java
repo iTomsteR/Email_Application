b32bd0d9c66ce1ce96f51c08065acb3823039d67
iTomster <tomsterman97@gmail.com>
2019-09-27 18:57:50 +0300

    Initial Commit

---
+++ src/Email.java
@@ -0,0 +1,16 @@
+public class Email {
+
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private String alternativeEmail;
+    private int mailboxCapacity;
+
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
+}
