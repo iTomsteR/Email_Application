4b7dd3ae4480cc004c350a6bf8729fd0c0ab9f35
iTomster <tomsterman97@gmail.com>
2019-09-28 10:53:56 +0300

    Ask the user for department.

--- src/Email.java
+++ src/Email.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Email {
 
     private String firstName;
@@ -10,6 +12,30 @@ public class Email {
     public Email(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+        System.out.println("Email created for user: " + this.firstName + " " + this.lastName);
+
+        //ask the user for his department
+        this.department = setDepartment();
+        System.out.println("Department: " + this.department);
+    }
+
+    //helper function
+    private String setDepartment() {
+        System.out.print("Please select your department code:\n0 - None\n1 - Sales\n2 - Accounting\n3 - Development\nEnter department code: ");
+        Scanner in = new Scanner(System.in);
+        int depChoice = in.nextInt();
+        switch(depChoice) {
+            case 0:
+                return "";
+            case 1:
+                return "sales";
+            case 2:
+                return "accounting";
+            case 3:
+                return "development";
+            default:
+                return "";
+        }
     }
 
 
